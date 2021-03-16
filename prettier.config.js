895e3173afa9156792c31044cb24c4260cd7bdb9
manonicu <gemini0525@foxmail.com>
2021-03-16 10:31:17 +0800

    更新配置

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "arrowParens": "avoid",
+  "bracketSpacing": false,
+  "jsxBracketSameLine": false,
+  "jsxSingleQuote": false,
+  "printWidth": 160,
+  "semi": true,
+  "singleQuote": false,
+  "tabWidth": 4,
+  "trailingComma": "none",
+  "useTabs": true
+}
