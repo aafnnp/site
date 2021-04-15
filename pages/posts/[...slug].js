3464ee35648791df3ce8476d15405a319f96eca8
Manon.icu <gemini0525@foxmail.com>
2021-04-15 14:46:22 +0800

    update

--- pages/posts/[...slug].js
+++ pages/posts/[...slug].js
@@ -1,32 +1,30 @@
-import { useRouter } from 'next/router';
-import { GetAllPosts, GetPostBySlug, GetRandomPost } from '../../getAllPosts';
-import dynamic from 'next/dynamic';
-import Head from 'next/head';
-import { connectToDatabase } from '../../utils/db';
+import {useRouter} from "next/router";
+import {GetAllPosts, GetPostBySlug, GetRandomPost} from "../../getAllPosts";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import {connectToDatabase} from "../../utils/db";
 // import _Post from '../../models/_Post'
 
 const Markdown = dynamic(
 	() => {
-		return import('../../components/Markdown');
+		return import("../../components/Markdown");
 	},
-	{ ssr: false },
+	{ssr: false}
 );
 
 const Random = dynamic(() => {
-	return import('../../components/RandomPost');
+	return import("../../components/RandomPost");
 });
 
 const Comments = dynamic(() => {
-	return import('../../components/Comments');
+	return import("../../components/Comments");
 });
 
-export default function Post({ post }) {
+export default function Post({post}) {
 	const router = useRouter();
-	const { data, content } = post;
-	const { tags, count } = data;
+	const {data, content} = post;
+	const {tags, count} = data;
 	const randomPost = GetRandomPost(tags);
-
-	console.log(router);
 
 	if (!router.isFallback && !post) {
 		return <div>fallback</div>;
@@ -40,13 +38,7 @@ export default function Post({ post }) {
 			<h1 className="font-bold text-2xl pb-2">{data.title}</h1>
 			<div className="post-info text-sm flex justify-between">
 				<div className="flex items-center">
-					<img
-						src="/github.svg"
-						alt="manonicu"
-						width="18"
-						height="18"
-						className="rounded-full mr-2"
-					/>
+					<img src="/github.svg" alt="manonicu" width="18" height="18" className="rounded-full mr-2" />
 					<span className="mr-2">Manon.icu</span>/ {data.date}（{data.fromNow}）
 				</div>
 				<span>{count} views</span>
@@ -63,33 +55,27 @@ export default function Post({ post }) {
 export async function getStaticPaths() {
 	const AllPosts = await GetAllPosts();
 	return {
-		paths: AllPosts.map((posts) => {
+		paths: AllPosts.map(posts => {
 			return {
 				params: {
-					slug: posts.link.substr(1).split('/'),
-				},
+					slug: posts.link.substr(1).split("/")
+				}
 			};
 		}),
-		fallback: false,
+		fallback: false
 	};
 }
 
-export async function getStaticProps({ params }) {
-	const { data, content } = await GetPostBySlug(params.slug);
+export async function getStaticProps({params}) {
+	const {data, content} = await GetPostBySlug(params.slug);
 
-	const { client, db } = await connectToDatabase();
+	const {client, db} = await connectToDatabase();
 	const isConnected = await client.isConnected();
 	if (isConnected) {
 		let result;
-		result = await db.collection('Post').findOne({ name: data.title });
-		await db
-			.collection('Post')
-			.updateOne(
-				{ name: data.title },
-				{ $set: { count: result ? +result.count + 1 : 1 } },
-				{ upsert: true },
-			);
-		result = await db.collection('Post').findOne({ name: data.title });
+		result = await db.collection("Post").findOne({name: data.title});
+		await db.collection("Post").updateOne({name: data.title}, {$set: {count: result ? +result.count + 1 : 1}}, {upsert: true});
+		result = await db.collection("Post").findOne({name: data.title});
 		data.count = result ? result.count : 0;
 	}
 
@@ -97,8 +83,8 @@ export async function getStaticProps({ params }) {
 		props: {
 			post: {
 				data,
-				content,
-			},
-		},
+				content
+			}
+		}
 	};
 }
