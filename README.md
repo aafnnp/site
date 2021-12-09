c977565331a5eed9064458133728c6c3c3964dad
Manon.icu <gemini0525@foxmail.com>
2021-12-09 11:11:17 +0800

    add devcard

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
+<a href="https://app.daily.dev/Manonicu"><img src="https://api.daily.dev/devcards/fc00d4e8de694d41a9175a42393b0e78.png?r=kmd" width="400" alt="Manon.icu's Dev Card"/></a>
+
 - https://manon.icu/
 - https://site-lk00kjo3z-manonicu.vercel.app/
 - https://site-jcqb.pages.dev/
--- components/Unsplash.jsx
+++ components/Unsplash.jsx
@@ -1,45 +1,29 @@
 import { Image } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export default class Unsplash extends React.Component {
-  state = {
-    photos: '',
-    imgHeight: 0,
-  };
-
-  componentDidMount() {
-    const imgWidth = document.querySelector('.chakra-container').clientWidth;
+export default function Unsplash(props) {
+  const [photos, setPhotos] = React.useState(
+    'https://source.unsplash.com/random'
+  );
+  useEffect(() => {
     fetch(
       `https://api.unsplash.com/search/photos/?client_id=rqPMZ8Ur7rQa6x2P1oPOSziry4m5XXod9KWStukAAy4&query=` +
-        this.props.tags[0] || 'coding'
+        props.tags[0] || 'coding'
     )
       .then((response) => response.json())
       .then(({ results }) => {
         const random = Math.floor(Math.random() * results.length);
-        this.setState({
-          photos: results[random].urls.regular,
-          imgHeight: imgWidth * 0.382,
-        });
-      })
-      .catch((_) => {
-        this.setState({
-          photos: 'https://source.unsplash.com/random',
-          imgHeight: imgWidth * 0.382,
-        });
+        setPhotos(results[random].urls.regular);
       });
-  }
+  }, []);
 
-  render() {
-    return (
-      <Image
-        borderRadius="md"
-        htmlWidth="100%"
-        fit="cover"
-        htmlHeight="auto"
-        src={this.state.photos}
-        loading="lazy"
-        alt={this.props.title}
-      />
-    );
-  }
+  return (
+    <Image
+      borderRadius="md"
+      aspectratio={1 / 0.382}
+      src={photos}
+      loading="lazy"
+      alt={props.title}
+    />
+  );
 }
