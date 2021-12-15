e5a076fa666a5ec7d112cbaca51f0adac808eb21
Manon.icu <gemini0525@foxmail.com>
2021-12-15 16:20:40 +0800

    dev

--- static/mkgND13_GUdE4TtB13DFR/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fchallenges","\u002Fblog\u002F[...slug]","\u002Fchallenges\u002F[...slug]","\u002Fplayground\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
