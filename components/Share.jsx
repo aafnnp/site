16277743a9e633083912f3f8282216ea30873c4d
pfan <gemini0525@foxmail.com>
2022-04-07 17:29:31 +0800

    chore: add social share

--- components/Share.jsx
+++ components/Share.jsx
@@ -8,36 +8,36 @@ import {
   RedditShareButton,
   RedditIcon,
   FacebookShareButton,
-  FacebookIcon,
+  FacebookIcon
 } from 'next-share'
 import {useRouter} from 'next/router'
+import {useState, useEffect} from 'react'
 
 export default function Share(props) {
   const {asPath} = useRouter()
-  console.log(props.data)
+  const [url, setUrl] = useState(asPath)
+  useEffect(() => {
+    setUrl(location.href)
+  }, [asPath])
   return (
     <>
       {asPath === '/' ? null : (
         <div className="share py-4 flex justify-center gap-2">
-          <TwitterShareButton url={location.href} title={props.data.title}>
+          <TwitterShareButton url={url} title={props.data.title}>
             <TwitterIcon size={24} round />
           </TwitterShareButton>
-          <WeiboShareButton url={location.href} title={props.data.title}>
+          <WeiboShareButton url={url} title={props.data.title}>
             <WeiboIcon size={24} round />
           </WeiboShareButton>
-          <EmailShareButton
-            url={location.href}
-            subject={props.data.title}
-            body="body"
-          >
+          <EmailShareButton url={url} subject={props.data.title} body="body">
             <EmailIcon size={24} round />
           </EmailShareButton>
-          <RedditShareButton url={location.href} title={props.data.title}>
+          <RedditShareButton url={url} title={props.data.title}>
             <RedditIcon size={24} round />
           </RedditShareButton>
           <FacebookShareButton
-            url={location.href}
-            quote={'next-share is a social share buttons for your next React apps.'}
+            url={asPath}
+            quote={props.data.title}
             hashtag={props.data.tags[0]}
           >
             <FacebookIcon size={24} round />
