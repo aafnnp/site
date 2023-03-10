0bcd559ec003d586a855921bd0531072b5164a18
pfan <gemini0525@foxmail.com>
2023-03-10 20:36:05 +0800

    :bug:fix: hidden nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,9 @@ const App = ({Component, pageProps, router}) => {
       </Head>
       <SEO url={url} />
       <div className={`grid grid-cols-1 sm:grid-cols-[300px_1fr]`}>
-        {!['/', '/resume'].includes(route) && <Menu />}
+        {route !== '/' && (
+          <Menu />
+        )}
         <Component {...pageProps} canonical={url} key={url} />
       </div>
       <Analytics />
