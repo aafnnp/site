1e51a23f70aa08156478b197cc3ed9a8170391bc
Manon.icu <gemini0525@foxmail.com>
2023-08-04 14:46:47 +0800

    fix: link style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
     './_playground/**/*.{js,ts,jsx,tsx}',
     './_challenges/**/*.{js,ts,jsx,tsx}'
   ],
