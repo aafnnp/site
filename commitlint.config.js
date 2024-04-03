ec8c62d1e23f70489c39565b81e264382a92e59c
pfan <gemini0525@foxmail.com>
2024-04-03 20:51:47 +0800

    Remove unused configuration files

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  formatter: '@commitlint/format',
-  ignores: [(commit) => commit === ''],
-  defaultIgnores: true,
-  helpUrl:
-    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint'
-}
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const {withContentlayer} = require('next-contentlayer')
-module.exports = withContentlayer({
-  // output: 'export',
-  reactStrictMode: true,
-  images: {
-    unoptimized: true
-  }
-})
